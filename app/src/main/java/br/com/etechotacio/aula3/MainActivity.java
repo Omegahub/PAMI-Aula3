6563a7826f50bc1fafd50a1d8451e8638adc371a
Omegahub <Omegahub2019@gmail.com>
2019-08-12 20:22:23 -0300

    Toast aula3

---
+++ app/src/main/java/br/com/etechotacio/aula3/MainActivity.java
@@ -0,0 +1,22 @@
+package br.com.etechotacio.aula3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onProcessar(View view)
+
+    {
+        Toast toast = Toast.makeText(this,"A vida é dura",Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
